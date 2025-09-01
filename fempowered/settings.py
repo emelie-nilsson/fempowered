36251f91eae16c80e018026e479e7274bc220fc2
emelie-nilsson <emelie.nilsson021@gmail.com>
2025-09-01 16:56:32 +0100

    Fix true to True in settings

--- fempowered/settings.py
+++ fempowered/settings.py
@@ -182,7 +182,7 @@ else:
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.sendgrid.net")
     EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
-    EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True").lower() == "true"
+    EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True").lower() == "True"
     EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "apikey")
     EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
     DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "noreply@your-domain.com")
