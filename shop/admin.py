ca0868b4d66c3df8ef26483437a8321f8a54bbb6
emelie-nilsson <emelie.nilsson021@gmail.com>
2025-09-12 15:33:02 +0100

    Add product, category and review to admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Product, Category, Review
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "category", "price", "is_new")
+    list_filter = ("category", "is_new")
+    search_fields = ("name", "description")
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug")
+    prepopulated_fields = {"slug": ("name",)}
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("product", "user", "rating", "created_on")
+    list_filter = ("rating", "created_on")
+    search_fields = ("body", "user__username", "product__name")
